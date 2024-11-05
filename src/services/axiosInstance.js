243461cd7778bb061fa4a00ae475eb38807453b8
LozpinLenyn <113066055+Pinedle@users.noreply.github.com>
2024-11-05 02:21:15 -0500

    subido front

---
+++ src/services/axiosInstance.js
@@ -0,0 +1,12 @@
+"use client";
+
+import axios from "axios";
+
+const axiosInstance = axios.create ({
+    baseURL: "https://api.gamescoremanager.com",
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export default axiosInstance;
